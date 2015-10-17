1a84011ac288b5ec043f2ef4443d0197176ed5a1
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-10-17 21:32:13 +0300

    Added recursive directory crawler for the ExcelFileReader

--- FurnitureFactory/FurnitureFactory.DataLoad/ExcelDataFeeder.cs
+++ FurnitureFactory/FurnitureFactory.DataLoad/ExcelDataFeeder.cs
@@ -28,7 +28,39 @@
         {
             if (this.CanLoad(directoryPath))
             {
+                DirectoryInfo currentPathInfo = new DirectoryInfo(directoryPath);
+                RecursiveDirectoryCrawler(currentPathInfo);
+            }
+        }
 
+        private void RecursiveDirectoryCrawler(DirectoryInfo root)
+        {
+            FileInfo[] files = null;
+
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+            }   
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                foreach (FileInfo fi in files)
+                {
+                    Console.WriteLine(fi.FullName);
+                }
+
+                subDirs = root.GetDirectories();
+
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    RecursiveDirectoryCrawler(dirInfo);
+                }
             }
         }
     }
