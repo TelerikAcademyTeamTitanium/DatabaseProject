910e165d6df5ba1c41077bb551d9083b8d2e4021
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-10-17 03:08:38 +0300

    Added initial VS solution and a ZIP reader.

---
+++ FurnitureFactory/FurnitureFactory.DataLoad/ZipFileReader.cs
@@ -0,0 +1,52 @@
+﻿namespace FurnitureFactory.DataLoad
+{
+    using System.IO;
+    using System.IO.Compression;
+
+    class ZipFileReader : IFileReader
+    {
+        private ZipFileReader()
+        {
+        }
+
+        public string ZipFilePath { get; private set; }
+
+        public static ZipFileReader Create()
+        {
+            return new ZipFileReader();
+        }
+
+        public void ReadFile(string path)
+        {
+            if (this.TryLoadFile(path))
+            {
+                this.ZipFilePath = path;
+
+                ZipArchive zip = ZipFile.OpenRead(this.ZipFilePath);
+
+                foreach (ZipArchiveEntry entry in zip.Entries)
+                {
+                    if (Path.GetFileName(entry.FullName).Equals(string.Empty))
+                    {
+                        zip.ExtractToDirectory(Directory.GetCurrentDirectory());
+                    }
+                }
+            }
+        }
+
+        public bool TryLoadFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Invalid file path!");
+            }
+
+            if (!Path.GetExtension(path).Equals(".zip"))
+            {
+                throw new FileLoadException("Provided file is not a zip archive!");
+            }
+
+            return true;
+        }
+    }
+}
