c2a0b111be0fec213ce04e4c5041419218dd954d
shunobaka <vesselin_minev@abv.bg>
2015-10-18 14:58:06 +0300

    Added DAO Interfaces for accessing the MongoDb data

---
+++ FurnitureFactory/FurnitureFactory.MongoDb.Data/Contracts/IClientsDAO.cs
@@ -0,0 +1,16 @@
+﻿namespace FurnitureFactory.MongoDb.Data.Contracts
+{
+    using Models;
+    using System.Collections.Generic;
+
+    internal interface IClientsDAO
+    {
+        void Insert(Client client);
+
+        ICollection<Client> GetAll();
+
+        void DeleteAll();
+
+        void Delete(Client client);
+    }
+}
