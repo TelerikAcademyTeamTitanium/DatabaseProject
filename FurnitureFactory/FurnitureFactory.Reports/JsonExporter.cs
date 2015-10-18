9834fadf6bcbac3d401fe421a1b3fd5696fc8f42
DennyGD <denny.dimitrova@abv.bg>
2015-10-18 23:49:59 +0300

    Added class for exporting json files

--- FurnitureFactory/FurnitureFactory.Reports/JsonExporter.cs
+++ FurnitureFactory/FurnitureFactory.Reports/JsonExporter.cs
@@ -1,6 +1,26 @@
 ﻿namespace FurnitureFactory.Reports
 {
-    public class JsonExporter
+    using System.Collections.Generic;
+    using System.IO;
+
+    using Newtonsoft.Json;
+    using Objects;
+
+    public class JsonExporter : FileExporter
     {
+        public JsonExporter(string outputPath, IList<Product> data)
+            : base(outputPath, data)
+        {
+        }
+
+        public override void Export()
+        {
+            foreach (var item in this.Data)
+            {
+                var serializedProduct = JsonConvert.SerializeObject(item, Formatting.Indented);
+                string fileName = item.Id + ".json";
+                File.WriteAllText(fileName, serializedProduct);
+            }
+        }
     }
 }
