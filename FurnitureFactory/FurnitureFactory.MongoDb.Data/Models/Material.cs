a45d4ac9ed198f9a971e2b95f70ca6c4d87e6bd3
shunobaka <vesselin_minev@abv.bg>
2015-10-18 13:15:30 +0300

    Adding Material model for the MongoDb data

---
+++ FurnitureFactory/FurnitureFactory.MongoDb.Data/Models/Material.cs
@@ -0,0 +1,15 @@
+﻿namespace FurnitureFactory.MongoDb.Data.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Material
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string MaterialId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Units { get; set; }
+    }
+}
