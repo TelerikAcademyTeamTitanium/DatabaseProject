477beab75dc66e761617ceb2d6e59db3fef6ac6b
shunobaka <vesselin_minev@abv.bg>
2015-10-18 13:15:46 +0300

    Adding Client model for the MongoDb data

---
+++ FurnitureFactory/FurnitureFactory.MongoDb.Data/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿namespace FurnitureFactory.MongoDb.Data.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Client
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public long Mobile { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contact { get; set; }
+    }
+}
