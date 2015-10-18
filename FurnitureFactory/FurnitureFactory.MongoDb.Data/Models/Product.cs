190ed2f82ea51a1ee647273c234d52a4c25c28c5
shunobaka <vesselin_minev@abv.bg>
2015-10-18 13:15:10 +0300

    Added Products model which will be used for MongoDb data

---
+++ FurnitureFactory/FurnitureFactory.MongoDb.Data/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace FurnitureFactory.MongoDb.Data.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Product
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Color { get; set; }
+    }
+}
