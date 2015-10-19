5b471e5e9e1e22cb6e4e8491dc3792dd1b2c0215
zdzdz <zdzdz.link@yahoo.com>
2015-10-19 22:26:26 +0300

    Added check for invalid outputPath.

--- FurnitureFactory/FurnitureFactory.XmlReporter/FileExporter.cs
+++ FurnitureFactory/FurnitureFactory.XmlReporter/FileExporter.cs
@@ -1,17 +1,36 @@
 ﻿namespace FurnitureFactory.XmlReporter
 {
     using System.Collections.Generic;
+    using System.IO;
+
     using FurnitureFactory.XmlReporter.Objects;
 
     public abstract class FileExporter
     {
+        private string outputPath;
+
         protected FileExporter(string outputPath, IList<Orders> data)
         {
             this.OutputPath = outputPath;
             this.Data = new List<Orders>(data);
         }
-        
-        public string OutputPath { get; set; }
+
+        public string OutputPath
+        {
+            get
+            {
+                return this.outputPath;
+            }
+            set
+            {
+                if (!Directory.Exists(value))
+                {
+                    throw new DirectoryNotFoundException(string.Format("{0} not found.", value));
+                }
+
+                this.outputPath = value;
+            }
+        }
 
         public IList<Orders> Data { get; set; }
 
