c8ea39f44aa05965c2d3819253b8a3ecbe936713
zdzdz <zdzdz.link@yahoo.com>
2015-10-19 13:35:00 +0300

    Added XmlExporter using LINQ to Xml

---
+++ FurnitureFactory/FurnitureFactory.XmlReporter/Objects/Orders.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace FurnitureFactory.XmlReporter.Objects
+{
+    using System;
+    using System.Xml.Linq;
+    using FurnitureFactory.Model;
+
+    public class Orders
+    {
+        public Orders(Client client, DateTime dueData, OrderStatus status)
+        {
+            this.Client = client;
+            this.DueData = dueData;
+            this.Status = status;
+        }
+
+        [XmlAttribute]
+        public Client Client { get; set; }
+
+        [XmlAttribute]
+        public DateTime DueData { get; set; }
+
+        [XmlAttribute]
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ FurnitureFactory/FurnitureFactory.XmlReporter/XmlExporter.cs
@@ -0,0 +1,29 @@
+﻿namespace FurnitureFactory.XmlReporter
+{
+    using System.Xml.Linq;
+    using System.Collections.Generic;
+    using FurnitureFactory.XmlReporter.Objects;
+
+    public class XmlExporter : FileExporter
+    {
+        public XmlExporter(string outputPath, IList<Orders> data)
+            : base(outputPath, data)
+        { 
+        }
+
+        public override void Export()
+        {
+            foreach (Orders order in this.Data)
+            {
+                XElement report =
+                    new XElement("sales",
+                        new XElement("sale", new XAttribute("client", order.Client), 
+                        new XElement("summary", 
+                            new XAttribute("dueDate", order.DueData),
+                            new XAttribute("status", order.Status))));
+
+                report.Save(this.OutputPath);
+            }
+        }
+    }
+}
