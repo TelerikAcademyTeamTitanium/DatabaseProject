e9eb3dd6a4cb6616a6ae01d43fb745a0c6531d37
iliantova <iliyana.antova@gmail.com>
2015-10-20 03:05:13 +0300

    3 A.M

--- FurnitureFactory/FurnitureFactory.ConsoleClient/Startup.cs
+++ FurnitureFactory/FurnitureFactory.ConsoleClient/Startup.cs
@@ -7,57 +7,30 @@
     using Utilities;
     using MongoDb.Data;
     using Importer;
-<<<<<<< HEAD
     using System.IO;
     using DataLoad;
     using System.Data;
-=======
     using XmlReporter;
     using Exporter;
     using Reports;
->>>>>>> refs/remotes/origin/master
+
 
     public class Startup
     {
         static void Main()
         {
-            // var db = new FurnitureFactoryDbContext();
-
-            //// var materials = new Material();
-            // var material = new Material
-            // {
-            //     Name = "Wood",
-            //     Unit = Units.SquareMeter
-            // };
-            // 
-            // var produkt = new Product
-            // {
-            //     Name = "Table",
-            //     Price = 150M
-            // };
-            // 
-            // var materialsperProduct = new MaterialsPerProduct
-            // {
-            //     MaterialId = material.MaterialId,
-            //     ProductId = produkt.ProductId,
-            //     QuantityMaterials = 2,
-            // };
-            // 
-            // 
-            // db.Products.Add(produkt);
-            // db.Materials.Add(material);
-            // db.MaterialsPerProduct.Add(materialsperProduct);
-            // 
-            // db.SaveChanges();
-
-            // Testing importer
-
+            
             MongoDbPopulator.PopulateFurnitureFactory();
             ImportMongoDbDataInMssql();
+            ImportExcelDataInMssql();
 
             var xmlReporter = new XmlExporter();
             var clients = new ClientsExporter(new FurnitureFactoryDbContext()).Export();
             xmlReporter.ExportClients(clients, "../../../../Exports/Xml/ClientsReport.xml");
+
+            var xmlReporterOrder = new XmlExporter();
+            var orders = new OrdersExporter(new FurnitureFactoryDbContext()).Export();
+            xmlReporterOrder.ExportOrders(orders, "../../../../Exports/Xml/OrdersReport.xml");
 
             var products = new ProductsExporter(new FurnitureFactoryDbContext()).Export();
             var jsonReporter = new JsonExporter(JsonExporter.DefaultOutputPath, products);
@@ -69,18 +42,23 @@
 
         private static void ImportMongoDbDataInMssql()
         {
-            /*
+            
             var clients = FurnitureFactoryMongoDbDAO.GetAllClients();
             var products = FurnitureFactoryMongoDbDAO.GetAllProducts();
             var materials = FurnitureFactoryMongoDbDAO.GetAllMaterials();
-            */
+
             var importer = new FurnitureFactoryMsSqlImporter();
-            
-            /*
+
             importer.ImportClients(clients);
             importer.ImportProducts(products);
             importer.ImportMaterials(materials);
-            */
+            
+        }
+
+        private static void ImportExcelDataInMssql()
+        {
+            var importer = new FurnitureFactoryMsSqlImporter();
+
             string zipFilePath = "../../../../ExcelTables/test.zip";
             FileInfo info = new FileInfo(zipFilePath);
             ZipFileReader zipReader = ZipFileReader.Create();
